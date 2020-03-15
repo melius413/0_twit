4d5559c798ccfe210dcf7d871c78fb07feb720b4
melius413 <melius413@gmail.com>
2020-03-15 17:33:47 +0900

    deploy

--- modules/mysql.js
+++ modules/mysql.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql2/promise');
 const connect = mysql.createPool({
-	host: "localhost",
-	port: 3307,
-	user: "root",
+	// host: "localhost",
+	host: "db.melius413.gabia.io",
+	// port: 3307,
+	port: 3306,
+	// user: "root",
+	user: "melius413",
 	password: process.env.dbpass,
-	database: 'twit',
+	// database: 'twit',
+	// database: 'twit',
 	connectionLimit: 10,
 	waitForConnections: true
 });
